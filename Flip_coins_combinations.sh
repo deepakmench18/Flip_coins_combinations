613554a80d2550a662f4845c41a46b02287e9363
deepakmench18 <deepakmench18@gmail.com>
2021-06-03 17:51:59 +0530

    Percentage of singlet combination

--- Flip_coins_combinations.sh
+++ Flip_coins_combinations.sh
@@ -1,6 +1,18 @@
 heads=1
 tails=0
+declare -A res
+
 toos=$((RANDOM%2))
+echo "Enter n for the loop "
+read n
+for (( i=0;i<n;i++ ))
+do
+   toos=$((RANDOM%2))
+   if [ $toos -eq $heads ]
+   then
+       res['heads']=$(("${res['heads']}"+1))
+   else
+       res['tails']=$(("${res['tails']}"+1))
 
 if [ $toos -eq $heads ]
 then
@@ -8,3 +20,12 @@ then
 else
    echo "Tails win "
 fi
+   fi
+done
+headcnt=$(($n-${res['heads']}))
+headpercent=$((($headcnt*100)/$n))
+tails=${res['tails']}
+heads=${res['heads']}
+
+echo " $tails and $head "
+echo " tails = $headpercent% and head = $((100-$headpercent))%" 

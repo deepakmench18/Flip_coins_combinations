5c1b594fcac01ca1497e9eaacf517d25e0931398
deepakmench18 <deepakmench18@gmail.com>
2021-06-03 17:57:28 +0530

    Doublet Combination as the percentage

--- Flip_coins_combinations.sh
+++ Flip_coins_combinations.sh
@@ -1,31 +1,49 @@
+
 heads=1
 tails=0
 declare -A res
 
-toos=$((RANDOM%2))
 echo "Enter n for the loop "
 read n
+hh=0
+ht=1
+th=2
+tt=3
 for (( i=0;i<n;i++ ))
 do
-   toos=$((RANDOM%2))
-   if [ $toos -eq $heads ]
+   toss=$((RANDOM%2))
+   if [ $toss -eq $heads ]
+   toss=$((RANDOM%4))
+   if [ $toss -eq $hh ]
+   then
+       res['hh']=$(("${res['hh']}"+1))
+   elif [ $toss -eq $ht ]
+   then
+       res['ht']=$(("${res['ht']}"+1))
+   elif [ $toss -eq $th ]
    then
        res['heads']=$(("${res['heads']}"+1))
+       res['th']=$(("${res['th']}"+1))
+
    else
        res['tails']=$(("${res['tails']}"+1))
+       res['tt']=$(("${res['tt']}"+1))
 
-if [ $toos -eq $heads ]
-then
-   echo "Heads win"
-else
-   echo "Tails win "
-fi
    fi
 done
 headcnt=$(($n-${res['heads']}))
 headpercent=$((($headcnt*100)/$n))
-tails=${res['tails']}
-heads=${res['heads']}
+echo "tails=${res['tails']} and heads=${res['heads']}"
+echo "occurance of tails = $headpercent% and head = $((100-$headpercent))%" 
+hhcnt="${res['hh']}"
+htcnt="${res['ht']}"
+ttcnt="${res['tt']}"
 
-echo " $tails and $head "
-echo " tails = $headpercent% and head = $((100-$headpercent))%" 
+hhpercent=$((($hhcnt*100)/$n))
+htpercent=$((($htcnt*100)/$n))
+ttpercent=$((($ttcnt*100)/$n))
+thpercent=$((100-$hhpercent-$htpercent-$ttpercent))
+headpercent=$((100-$ttpercent-($thpercent+$htpercent)/2))
+echo "hh=${res['hh']}, ht=${res['ht']}, th=${res['th']} and tt=${res['tt']}"
+echo "hh=$hhpercent%, ht=$htpercent%, th=$thpercent% and tt=$ttpercent%"
+echo "heads=$headpercent% and tails=$((100-$headpercent))%"
